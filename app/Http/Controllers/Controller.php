b17549f204f5409dd83b4dc75eec2fe70b999b33
Kyle Alejandro Martinez Lopez <kylealejandro@hotmail.com>
2023-10-19 20:00:40 -0600

    Documentacion generada de Swagger 8.5
    Modificacion de route y controladores

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API de Kyle Alejandro Martinez Lopez",
+ *      description="API de ejemplo para clase de desarrollo de BackEnd y APIs",
+ *      @OA\Contact(
+ *          email="kylealejandro@hotmail.com",
+ *          name="Kyle Alejandro Martinez Lopez",
+ *          url="https://localhost/api/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
